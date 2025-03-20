6540d2801be9506ec7262b6d948492d0f080cfdc
Monday1997 <axu1997@qq.com>
2025-03-20 15:43:21 +0800

    feat: init home pages

---
+++ src/components/home-menu/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-menu
+    theme="dark"
+    v-model:selectedKeys="selectedKeys"
+    style="width: 100%"
+    mode="vertical"
+    :items="items"
+    @click="handleClick"
+  />
+</template>
+
+<script setup lang="ts">
+import type { MenuProps } from 'ant-design-vue'
+import { ref } from 'vue'
+
+const selectedKeys = ref(['table-merge'])
+const items = ref([
+  {
+    key: 'table-merge',
+    label: '合并表格',
+    title: 'Navigation One',
+  },
+])
+const handleClick: MenuProps['onClick'] = (menuInfo) => {
+  console.log('click ', menuInfo)
+}
+</script>
+
+<style scoped></style>
