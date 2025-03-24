d3c8a2a7bf4b19c15ddbe84723cf88f92a60dc9d
Monday1997 <axu1997@qq.com>
2025-03-24 15:55:22 +0800

    feat: reconstruction menu & routes

--- src/Layout/home-menu/config.ts
+++
@@ -1,3 +0,0 @@
-export const MenuRouteConfig = {
-  mergeTableRows1: 'merge-table-rows1',
-}
--- src/Layout/home-menu/index.vue
+++ src/Layout/home-menu/index.vue
@@ -1,27 +1,55 @@
 <template>
   <a-menu
     theme="dark"
+    mode="inline"
     v-model:selectedKeys="selectedKeys"
+    v-model:openKeys="openKeys"
     style="width: 100%"
-    mode="vertical"
     :items="items"
     @click="handleClick"
   />
 </template>
 
 <script setup lang="ts">
-import type { MenuProps } from 'ant-design-vue'
-import { MenuRouteConfig } from './config.ts'
+import { pagesRoute } from '@/router/index'
+import type { TMenuRouteRaw } from '@/router/index'
 const route = useRoute()
-const selectedKeys = ref([route.meta.menuName || route.name])
-const items = [
-  {
-    key: MenuRouteConfig.mergeTableRows1,
-    label: '合并表格1',
-  },
-]
-const handleClick: MenuProps['onClick'] = (menuInfo) => {
-  console.log('click ', menuInfo)
+const router = useRouter()
+
+const selectedKeys = ref([route.name])
+const openKeysMap: Record<string, string[]> = {}
+const items = getMenu(pagesRoute)
+
+type TMenu = {
+  key: string
+  label: string
+  children: TMenu[] | null
+}
+/**
+ * 构造menu
+ * @param routes
+ * @param opeKey 父组件的key集合 用于openKey的赋值
+ */
+function getMenu(routes: TMenuRouteRaw[], opeKey: string[] = []): TMenu[] {
+  return routes.map((item: TMenuRouteRaw) => {
+    if (opeKey.length) {
+      openKeysMap[item.name] = opeKey
+    }
+    return {
+      key: item.name,
+      label: item.meta.title,
+      children: item.children
+        ? getMenu(item.children as TMenuRouteRaw[], opeKey.concat(item.name))
+        : null,
+    }
+  })
+}
+const openKeys = openKeysMap[route.name as string]
+
+function handleClick(item: TMenu) {
+  router.push({
+    name: item.key,
+  })
 }
 </script>
 
