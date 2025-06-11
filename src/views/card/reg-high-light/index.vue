67d32327829af1fff6ea42c656bc38512fcc2353
chenxujian <axu1997@qq.com>
2025-06-11 12:04:09 +0800

    feat: 样式修改

--- src/views/card/reg-high-light/index.vue
+++ src/views/card/reg-high-light/index.vue
@@ -13,25 +13,27 @@
         src="https://img95.699pic.com/element/40029/9841.png_860.png"
       />
     </div>
-    <!-- 弹出的卡片 -->
-    <div
-      v-click-outside="hideCard"
-      class="card"
-      v-show="card.show"
-      :style="`left:${card.x}px;top:${card.y}px`"
-    >
-      {{ card.text }}
-    </div>
+    <Transition name="card">
+      <!-- 弹出的卡片 -->
+      <div
+        v-click-outside="hideCard"
+        class="card"
+        v-show="card.show"
+        :style="`left:${card.x}px;top:${card.y}px`"
+      >
+        {{ card.text }}
+      </div>
+    </Transition>
   </div>
 </template>
 
 <script setup lang="ts">
-const mapData: Record<string, number> = {
-  感冒: 0,
-  三九: 1,
-  小柴胡: 2,
-  发烧: 3,
-  咳嗽: 4,
+const mapData: Record<string, string> = {
+  感冒: '感冒是指百姓所说的“普通感冒”，又称“伤风”...',
+  三九: '999感冒灵颗粒为浅棕色至深棕色的颗粒，味甜...',
+  小柴胡: '小柴胡颗粒，中成药名。为和解剂，具有解表散热...',
+  发烧: '发热，俗称发烧，症状名，正常...',
+  咳嗽: '咳嗽（cough）是一种呼吸道常见症状，由...',
 }
 const card = reactive({ show: false, x: 0, y: 0, text: '', id: -1 })
 const textData = ref<string[]>([])
@@ -62,12 +64,10 @@ function init() {
   textData.value = returnData
 }
 function showCard(item: string, e: MouseEvent) {
-  const id = mapData[item]
   card.show = true
   card.x = e.clientX
   card.y = e.clientY
-  card.id = id
-  card.text = `根据id值${id}请求回来的数据展示`
+  card.text = mapData[item] || `根据id值${id}请求回来的数据展示`
   console.log(e.clientX)
 }
 function hideCard() {
@@ -75,7 +75,7 @@ function hideCard() {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 .text_box {
   display: flex;
   align-items: start;
@@ -87,29 +87,35 @@ function hideCard() {
   width: 20px;
   vertical-align: middle;
   margin-top: -10px;
-  /* margin-bottom: 20px; */
+  cursor: pointer;
 }
 .text_box .card {
-  width: 100px;
-  height: 100px;
+  width: 120px;
   background-color: #bfc;
   border-radius: 20px;
   position: absolute;
   padding: 10px;
-  font-size: 18px;
   color: #ff0000;
-  animation: mymove 0.3s 1;
-  transition: all 0.5s ease-in-out;
 }
-@keyframes mymove {
-  from {
-    height: 0;
-    font-size: 0;
-  }
-  to {
-    height: 100px;
-    font-size: 18px;
-  }
+.card {
+  font-size: 14px;
+  min-height: 100px;
+  transition:
+    opacity 0.8s,
+    font-size 0.3s ease,
+    left 0.5s ease,
+    min-height 0.3s;
+}
+
+.card-enter-from,
+.card-leave-to {
+  opacity: 0;
+  min-height: 0;
+  font-size: 0;
+}
+.card-enter-to,
+.card-leave-from {
+  font-size: 18px;
 }
 .hightLight {
   color: red;
