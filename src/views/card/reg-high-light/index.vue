61b2d0b6c9600b3343f67a50dbf6530da059a239
Monday1997 <axu1997@qq.com>
2025-06-11 14:57:10 +0800

    feat: 拆分hook复用

--- src/views/card/reg-high-light/index.vue
+++ src/views/card/reg-high-light/index.vue
@@ -28,46 +28,25 @@
 </template>
 
 <script setup lang="ts">
-const mapData: Record<string, string> = {
-  感冒: '感冒是指百姓所说的“普通感冒”，又称“伤风”...',
-  三九: '999感冒灵颗粒为浅棕色至深棕色的颗粒，味甜...',
-  小柴胡: '小柴胡颗粒，中成药名。为和解剂，具有解表散热...',
-  发烧: '发热，俗称发烧，症状名，正常...',
-  咳嗽: '咳嗽（cough）是一种呼吸道常见症状，由...',
+import useSplitWrords from './use-split-words'
+import { mapData } from './config'
+
+const keys: string[] = []
+for (const key in mapData) {
+  keys.push(key)
 }
+// 根据map动态填写规则
+// 今天突然感冒了，请问吃三九好还是小柴胡好
+const str = '这是一份买车险'
+
+const { textData } = useSplitWrords(str, keys)
+
 const card = reactive({ show: false, x: 0, y: 0, text: '', id: -1 })
-const textData = ref<string[]>([])
-
-init()
-function init() {
-  const a = []
-  for (const key in mapData) {
-    a.push(key)
-  }
-  // 根据map动态填写规则
-  const str = '今天突然感冒了，请问吃三九好还是小柴胡好'
-  const reg = new RegExp(`(${a.join('|')})`, 'g')
-
-  let result: RegExpExecArray | null = null
-  let currentIndex = 0
-  const returnData: string[] = []
-
-  while ((result = reg.exec(str))) {
-    returnData.push(str.slice(currentIndex, result.index))
-    returnData.push(result[1])
-    currentIndex = result.index + result[0].length
-  }
-  if (str.length > currentIndex) {
-    returnData.push(str.slice(currentIndex, str.length))
-  }
-  console.log(returnData)
-  textData.value = returnData
-}
 function showCard(item: string, e: MouseEvent) {
   card.show = true
   card.x = e.clientX
   card.y = e.clientY
-  card.text = mapData[item] || `根据id值${id}请求回来的数据展示`
+  card.text = mapData[item] || `该词条待完善`
   console.log(e.clientX)
 }
 function hideCard() {
@@ -101,7 +80,7 @@ function hideCard() {
   font-size: 14px;
   min-height: 100px;
   transition:
-    opacity 0.8s,
+    opacity 0.3s,
     font-size 0.3s ease,
     left 0.5s ease,
     min-height 0.3s;
