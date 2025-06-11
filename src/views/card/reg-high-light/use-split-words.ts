abb041b314ed1dfef979d6dd61f11f6d72f9e4f7
chenxujian <axu1997@qq.com>
2025-06-11 15:05:47 +0800

    feat: 正则方式

--- src/views/card/reg-high-light/use-split-words.ts
+++ src/views/card/reg-high-light/use-split-words.ts
@@ -14,12 +14,13 @@ function useSplitWrords(str: string, keysOrReg: RegExp | string[]): TReturn {
   const textData = ref<string[]>([])
   const reg: RegExp = isReg(keysOrReg) ? keysOrReg : new RegExp(`(${keysOrReg.join('|')})`, 'g')
   let result: RegExpExecArray | null = null
-  let currentIndex = 0
+  let currentIndex = 0, regIndex = -1
   const returnData: string[] = []
-  while ((result = reg.exec(str)) ) {
+  while ((result = reg.exec(str)) && regIndex !== result!.index) {
     returnData.push(str.slice(currentIndex, result.index))
     returnData.push(result[1])
     currentIndex = result.index + result[0].length
+    regIndex = result.index
   }
   if (str.length > currentIndex) {
     returnData.push(str.slice(currentIndex, str.length))
