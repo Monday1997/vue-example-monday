743ce28962fa703b36cdbe6886e45a017fd6a1d7
chenxujian <axu1997@qq.com>
2025-06-21 09:51:04 +0800

    feat: 静态合并表格

---
+++ src/utils/tools/table-utils.ts
@@ -0,0 +1,57 @@
+import type { TableColumnType } from 'ant-design-vue';
+
+
+const prefix = 'column_span_';
+
+/**
+ * 合并列数据并将字段整合至list
+ * @param columnSpans string[] 要设置的列（必须连贯）
+ * @param list any[]
+ * */
+export function addSpansValueInList(columnSpans: string[], list: any[]): any[] {
+  calculateRowSpans(columnSpans, list);
+  return list;
+
+}
+
+// 计算每一列需要的rowsSpans 要合并的columnSpan
+function calculateRowSpans(columnSpans: string[], list: any[]): void {
+  const rows = list;
+  //遍历到当前列为止的行值
+  const columnValues: string[] = new Array(rows.length).fill('');
+  columnSpans.forEach(columnSpan => {
+    columnValues[0] += rows[0][columnSpan] + '_';
+    let currentSpanStart = 0;
+    for (let i = 0; i < rows.length; i++) {
+      if (i < rows.length - 1) {
+        columnValues[i + 1] += rows[i + 1][columnSpan] + '_';
+      }
+      // 遇到不同组或最后一行时结算跨度
+      if (i === rows.length - 1 || columnValues[i + 1] !== columnValues[i]) {
+        rows[currentSpanStart][prefix + columnSpan] = i - currentSpanStart + 1;
+        for (let j = currentSpanStart + 1; j <= i; j++) {
+          rows[j][prefix + columnSpan] = 0;
+        }
+        currentSpanStart = i + 1;
+      }
+    }
+  });
+}
+
+/**
+ * 结合setFormatHandle 设置要合并的行
+ * @param row_key string 对应的列名
+ * @param customCell 若原先有customCell
+ * */
+type CustomCellType = TableColumnType extends { customCell?: infer T } ? T : never;
+
+export function setCustomCell(row_key: string, customCell?: CustomCellType): CustomCellType {
+  return (record, rowIndex, column) => {
+    const baseCell = typeof customCell === 'function' ? customCell(record, rowIndex, column) : {};
+    return {
+      ...baseCell,
+      rowSpan: record[prefix + row_key],
+    };
+  };
+}
+
---
+++ src/views/mergeTableRows/merger-form-static.vue
@@ -0,0 +1,13 @@
+<template>
+  <PageLayout :navs="['合并表格', '合并表格(静态列)']" title="合并表格(静态列)">
+    <a-table bordered :columns="staticColumns" :dataSource="list"></a-table>
+  </PageLayout>
+</template>
+
+<script setup lang="ts">
+import { addSpansValueInList } from '@/utils/tools/table-utils'
+import { list, staticColumns } from './config'
+addSpansValueInList(['scheme', 'attr1', 'attr2', 'attr3'], list)
+</script>
+
+<style scoped></style>
