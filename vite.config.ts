cf3d15b367c41cbc33b9bc5b97ee3728ecd78365
Monday1997 <axu1997@qq.com>
2025-06-28 23:57:30 +0800

    Create main.yml
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    Update main.yml
    
    feat: 修改base

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,38 @@
 import { fileURLToPath, URL } from 'node:url'
-import Components from 'unplugin-vue-components/vite';
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    Components({
-      dirs: ['./src/Layout/page-layout'],
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: false, // css in js
-        }),
-      ],
-    }),
-    AutoImport({
-      include: [
-        /\.vue$/,
-        /\.vue\.[tj]sx?\?vue/
-      ],
-      imports: [
-        'vue',
-        'vue-router',
-      ],
-      dts: true
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      Components({
+        dirs: ['./src/Layout/page-layout'],
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false, // css in js
+          }),
+        ],
+      }),
+      AutoImport({
+        include: [/\.vue$/, /\.vue\.[tj]sx?\?vue/],
+        imports: ['vue', 'vue-router'],
+        dts: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    base: mode === 'development' ? '/' : '/vue-example-monday/',
+  }
 })
